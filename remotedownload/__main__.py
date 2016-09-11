575f40da441474f04f8d60f2f91bbdc76e92246b
Jon Colverson <github@vcxz.co.uk>
2016-09-11 23:27:03 +0100

    Made use of the downloading file size (if available in the headers) for the progress reporting

--- remotedownload/__main__.py
+++ remotedownload/__main__.py
@@ -86,12 +86,13 @@ class Downloader:
 
         response.raise_for_status()
 
+        total_file_size = int(response.headers.get('Content-Length', 0))
         total_bytes_written = 0
         for chunk in response.iter_content(chunk_size):
             out_file.write(chunk)
             total_bytes_written += len(chunk)
-            if progress_reporter: progress_reporter(total_bytes_written, 0)
-        if progress_reporter: progress_reporter.finish()
+            if progress_reporter: progress_reporter(total_bytes_written, total_file_size)
+        if progress_reporter and total_file_size == 0: progress_reporter.finish()
 
         filename = None
 
